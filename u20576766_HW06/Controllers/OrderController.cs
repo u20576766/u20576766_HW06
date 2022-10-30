87dc68b52356ba6caf41e58b5130cd9c80bdf0e8
u20576766 <u20576766@tuks.co.za>
2022-10-30 23:36:13 +0200

    Code from beginning

---
+++ u20576766_HW06/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using u20576766_HW06.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace u20576766_HW06.Controllers
+{
+    public class OrderController : Controller
+    {
+        private BikeStoresEntities db = new BikeStoresEntities();
+        // GET: Order
+        public ActionResult Orders(DateTime? search, int? tale)
+        {
+            List<order_items> orderI = db.order_items.ToList();
+            return View(db.order_items.Where(x => x.order.order_date == search || search == null).ToList().ToPagedList(1, 10));
+           
+        }
+    }
+}
---
+++ u20576766_HW06/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u20576766_HW06.MvcApplication" Language="C#" %>
